28662d520914c36df7ec5b20989f07ef05c23f9c
LifelongYuan <zhb_00@126.com>
2022-04-13 02:46:59 +0800

    change model to unicycle

--- user/Cal_Control_Matrix.m
+++ user/Cal_Control_Matrix.m
@@ -1,54 +1,40 @@
 
-function A_final = Cal_Control_Matrix(agent_number,original_poses)
-n = agent_number;  % number of agent
-E1 = zeros(2*n,1);  % core base 1
-E2 = zeros(2*n,1);  % core base 1
-for i=1:n
-    E1(2*i-1) = 1;
-    E2(2*i) = 1;
-end
+function A_final = Cal_Control_Matrix(agent_number,graph_matrix_semi)
+%% calculate kernalspace base vector.
+    n = agent_number;  % number of agent
+    E1 = zeros(2*n,1);  % core base 1
+    E2 = zeros(2*n,1);  % core base 2
+    for i=1:n
+        E1(2*i-1) = 1;
+        E2(2*i) = 1;
+    end
 
-q_star = [0;(3^0.5);1;(3^0.5);1.5;(3^0.5)/2;1;0;0;0;-0.5;(3^0.5)/2];
-rotate_theta = zeros(2,2,n);
-for i=1:n
-    rotate_theta(:,:,i) = [cos(original_poses(3,i)),-sin(original_poses(3,i));sin(original_poses(3,i)),cos(original_poses(3,i))];
-end
-world_2_local = blkdiag(rotate_theta(:,:,1),rotate_theta(:,:,2),rotate_theta(:,:,3),rotate_theta(:,:,4),rotate_theta(:,:,5),rotate_theta(:,:,6));
-% q_star_local = world_2_local*q_star;
-rotate_90 = [0,1;-1,0];
-rotate_90_block = blkdiag(rotate_90,rotate_90,rotate_90,rotate_90,rotate_90,rotate_90);
-q_star_perpendicular = rotate_90_block*q_star;
+    q_star = [0;(3^0.5);1;(3^0.5);1.5;(3^0.5)/2;1;0;0;0;-0.5;(3^0.5)/2];  % core base 3
+    rotate_90 = [0,1;-1,0];
+    rotate_90_block = blkdiag(rotate_90,rotate_90,rotate_90,rotate_90,rotate_90,rotate_90);  
+    q_star_perpendicular = rotate_90_block*q_star;  % core base 4
 
-N = [q_star,q_star_perpendicular,E1,E2];
-[U,S,V] = svd(N);
-U_last = U(:,5:end);
+    N = [q_star,q_star_perpendicular,E1,E2];  % kernal space
+    [U,S,V] = svd(N);
+    U_last = U(:,5:end);
 
-graph_matrix = [1,1,0,1,0,1;
-                0,1,1,0,1,0;
-                0,0,1,1,1,1;
-                0,0,0,1,1,0;
-                0,0,0,0,1,1;
-                0,0,0,0,0,1];
-            % graph_matrix = [1,1,1,1,1,1;
-            %     0,1,1,1,1,1;
-            %     0,0,1,1,1,1;
-            %     0,0,0,1,1,1;
-            %     0,0,0,0,1,1;
-            %     0,0,0,0,0,1];
-graph_matrix = graph_matrix.'+graph_matrix;
-cvx_begin
-    variable A(2*n,2*n) symmetric 
-    maximize(lambda_min(U_last' * A * U_last))
-    subject to
-        A * N == 0;
-        A(graph_matrix==0) == 0;
-        for row_index=1:n
-            for col_index=1:n
-                A(2*row_index-1,2*col_index-1) == A(2*row_index,2*col_index);
-                A(2*row_index,2*col_index-1) == -1*A(2*row_index-1,2*col_index);
+%% calculate graph_matrix
+
+    graph_matrix = graph_matrix_semi.' + graph_matrix_semi;
+
+%% convex optimization
+    cvx_begin
+        variable A(2*n,2*n) symmetric 
+        maximize(lambda_min(U_last' * A * U_last))
+        subject to
+            A * N == 0;
+            A(graph_matrix==0) == 0;
+            for row_index=1:n
+                for col_index=1:n
+                    A(2*row_index-1,2*col_index-1) == A(2*row_index,2*col_index);
+                    A(2*row_index,2*col_index-1) == -1*A(2*row_index-1,2*col_index);
+                end
             end
-        end
-cvx_end
-eig(A)
-A_final = A;
+    cvx_end
+    A_final = A;
 end
--- user/formation.m
+++ user/formation.m
@@ -1,77 +1,69 @@
-%% EXAMPLE: Multi-Robot Swarm Behavior
-% Copyright 2018 The MathWorks, Inc.
 clear;
-%% Create a multi-robot environment
+%% Initialization
 numRobots = 6;
 env = MultiRobotEnv(numRobots);
 env.robotRadius = 0.15;
-env.showTrajectory = false;
-graph_matrix = [1,1,0,1,0,1;
+env.showTrajectory = true;
+graph_matrix_semi = [1,1,0,1,0,1;
                 0,1,1,0,1,0;
                 0,0,1,1,1,1;
                 0,0,0,1,1,0;
                 0,0,0,0,1,1;
                 0,0,0,0,0,1];
-%% Initialization
-% Number of robot teams
-sampleTime = 0.1;              % Sample time [s]
+sampleTime = 0.01;              % Sample time [s]
 tVec = 0:sampleTime:10000;        % Time array                
 
 % Initialize poses randomly, and add bias to each "team"
 poses = [10*(rand(2,numRobots) - 0.5); ...
          pi*rand(1,numRobots)];
-original_poses = poses;
+vel_xy = zeros(2,numRobots);
+vel_vw = zeros(2,numRobots);
+d_poses = zeros(3,numRobots);
+h_matrix= zeros(2,2);
+
+Control_Matrix = Cal_Control_Matrix(6,graph_matrix_semi);
+
 %% Simulation loop
-vel = zeros(3,numRobots);
-Control_Matrix = Cal_Control_Matrix(6,original_poses);
 for idx = 2:numel(tVec)
     % Update the environment
     env(1:numRobots, poses);
-%     xlim([-8 8]);   % Without this, axis resizing can slow things down
-%     ylim([-8 8]); 
-    axis equal;
-    % Read the sensor and execute the controller for each robot
+    xlim([-8 8]);   % Without this, axis resizing can slow things down
+    ylim([-8 8]);
+
     for rIdx = 1:numRobots
-       vel(1:2,rIdx) = swarmTeamController(poses,rIdx,Control_Matrix,original_poses);
+        h_matrix(:,:) = [   cos(poses(3,rIdx)),...
+                            sin(poses(3,rIdx));
+                            -sin(poses(3,rIdx)),...
+                            cos(poses(3,rIdx)) 
+                        ];
+        vel_xy(:,rIdx) = swarmTeamController(poses,rIdx,Control_Matrix);
+        vel_vw(:,rIdx) = h_matrix  * vel_xy(:,rIdx);
+        d_poses(:,rIdx) = [cos(poses(3,rIdx)),0;sin(poses(3,rIdx)),0;0,1] * vel_vw(:,rIdx);
     end
     
     % Discrete integration of pose
-    poses = poses + vel*sampleTime;
-%     if idx>100
-%         plot_connection(graph_matrix,poses)
-%     end
+    poses = poses + d_poses*sampleTime;
+
+    if idx>1000
+        plot_connection(graph_matrix_semi,poses)
+    end
 end
 
 %% Helper function: Robot Controller Logic
-function vel = swarmTeamController(poses,rIdx,control_m,original_poses)
-    
-    % Unpack the robot's pose and team index
-    % If there are no detections, turn in place
-    % v = 0;
-    % w = 2;
+function [vel_xy] = swarmTeamController(poses,rIdx,control_m)
     vel_xy = [0;0];
     [row,col]=size(poses);
-    
     for neighbor_index = 1:col
         if neighbor_index ~= rIdx
-            control_m(2*rIdx-1:2*rIdx,2*neighbor_index-1:2*neighbor_index)
-            % pose_local = [cos(original_poses(3,rIdx)),sin(original_poses(3,rIdx));-sin(original_poses(3,rIdx)),cos(original_poses(3,rIdx))]*(poses(1:2,neighbor_index) - poses(1:2,rIdx));
-            pose_local =(poses(1:2,neighbor_index) - poses(1:2,rIdx));
-            vel_xy = vel_xy + control_m(2*rIdx-1:2*rIdx,2*neighbor_index-1:2*neighbor_index)*-pose_local;
+            pose_diff =(poses(1:2,neighbor_index) - poses(1:2,rIdx));
+            vel_xy = vel_xy + control_m(2*rIdx-1:2*rIdx,2*neighbor_index-1:2*neighbor_index)*-pose_diff;
         end
     end
-    rIdx
-    vel = vel_xy
-    norm_vel = norm(vel);
-%     if norm_vel<1
-%         vel=[0;0];
-%     end
-    vel = vel ./ norm_vel;
-    % Convert to global velocity
-    % vel = bodyToWorld([v;0;w],pose);
-
+    norm_vel = norm(vel_xy);
+    vel_xy = vel_xy ./ norm_vel;
 end
 
+%% plot the connection 
 function plot_connection(graph_matrix,poses)
     [row,col]=size(poses);
     for index_1=1:col
@@ -85,5 +77,4 @@ function plot_connection(graph_matrix,poses)
             end
         end
     end
-
 end
